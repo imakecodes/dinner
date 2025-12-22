41a064cf6088327f870315f5015f81ddaa671bbf
Michel Wilhelm <michelwilhelm@gmail.com>
2025-12-22 14:59:17 +0000

    chore: add pnpm-workspace.yaml to manage workspace dependencies and ignore specific built dependencies feat(vite.config.ts): add allowedHosts configuration to server settings for improved host management

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+	allowedHosts: ['cube', '127.0.0.1', 'localhost'],
       },
       plugins: [react()],
       define: {
