8fd5ac3e5c0521a3b5478140ae86110c4a637e99
Michel Wilhelm <michelwilhelm@gmail.com>
2026-01-09 14:24:27 -0300

    DINNER-71: credenciais inválidas (#52)
    … reset-password APIs for error messages and responses
    
    fix(locales): add missing authentication error messages in pt-BR
    translation to improve user feedback

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 const nextJest = require('next/jest')
- 
+
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
- 
+
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
@@ -19,7 +19,8 @@ const customJestConfig = {
   transformIgnorePatterns: [
     '/node_modules/(?!(@google/genai|@google/generative-ai)/)'
   ],
+  modulePathIgnorePatterns: ['<rootDir>/.next/'],
 }
- 
+
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig)
